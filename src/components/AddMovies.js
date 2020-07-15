43ec7cfbe270c826a2d24629c67516db6d678ed3
alo-faith <alo.faith@gmail.com>
2020-07-15 14:32:45 +0300

    valid movie name

--- src/components/AddMovies.js
+++ src/components/AddMovies.js
@@ -6,24 +6,32 @@ import movieStore from "../store/movieStore";
 
 const AddMovies = () => {
   const [movie, setMovie] = useState({ name: "" });
+  const [msg, setMsg] = useState("");
 
   const handelChange = (event) => {
     const newMovie = { ...movie, [event.target.name]: event.target.value };
     setMovie(newMovie);
+    setMsg("");
   };
 
   const handelSubmit = (event) => {
     event.preventDefault();
-    movieStore.addMovie(movie);
+    const checkMovie = (movies) => {
+      return movie.name === movies.name;
+    };
+
+    const movieExists = movieStore.movies.some(checkMovie);
+
+    !movieExists ? movieStore.addMovie(movie) : setMsg("movie is exists ");
     document.forms["addForm"]["name"].value = "";
     document.forms["addForm"]["image"].value = "";
   };
 
   return (
-    <form id="addForm" onSubmit={handelSubmit}>
+    <form id="addForm" onSubmit={handelSubmit} style={{ width: "500px" }}>
       <div
         class="input-group  input-group-sm mb-3"
-        style={{ width: "300px", marginLeft: "5px" }}
+        style={{ marginLeft: "5px", display: "block" }}
       >
         <div className="form-group row">
           <input
@@ -35,7 +43,8 @@ const AddMovies = () => {
             required
             onSubmit="this.value =``"
           />
-        </div>
+        </div>{" "}
+        <p style={{ color: "red" }}>{msg}</p>
         <div className="form-group row">
           <input
             type="text"
@@ -47,10 +56,9 @@ const AddMovies = () => {
             onSubmit="this.value =``"
           />
         </div>
-
         <div className="form-group">
           <button
-            class="input-group-text"
+            class="input-group-text  "
             id="basic-addon2"
             type="submit"
             onSubmit={handelSubmit}
